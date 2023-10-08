b341ad61220eeb45a0059f54d6ebe1bd7535f328
Alex <alextrieu52@gmail.com>
2023-10-08 14:57:05 -0700

    Add navigation link to PRoductCard from Popular Products

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home";
-
+import ProductListing from "./components/ProductListing/ProductListing";
 function App() {
   return (
-    <>
-      <Header />
-      <Home />
-    </>
+    <Router>
+      <Header /> {/* Regardless of the page it is on, the Header is always present */}
+      <Routes>
+        <Route path="/product/:productId" element={<ProductListing />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,4 +1,5 @@
 import { Product } from "../../types/types";
+import { Link } from "react-router-dom";
 
 type ProductCardProps = {
   product: Product;
@@ -6,17 +7,19 @@ type ProductCardProps = {
 
 export default function ProductCard({ product }: ProductCardProps) {
   return (
-    <article key={product._id} className="flex-none flex flex-col items-center gap-1 w-40">
-      <div className="w-full h-64 relative">
-        <img
-          src={product.imageLink}
-          alt={product.name}
-          className="absolute top-0 left-0 w-full h-full object-contain"
-        />
-      </div>
-      <h2 className="text-center line-clamp-1">{product.name}</h2>
-      <p className="text-center line-clamp-2 h-12">{product.tagline}</p>
-      <p className="font-semibold">From ${product.price}</p>
-    </article>
+    <Link to={`/product/${product._id}`}>
+      <article key={product._id} className="flex-none flex flex-col items-center gap-1 w-40">
+        <div className="w-full h-64 relative">
+          <img
+            src={product.imageLink}
+            alt={product.name}
+            className="absolute top-0 left-0 w-full h-full object-contain"
+          />
+        </div>
+        <h2 className="text-center line-clamp-1">{product.name}</h2>
+        <p className="text-center line-clamp-2 h-12">{product.tagline}</p>
+        <p className="font-semibold">From ${product.price}</p>
+      </article>
+    </Link>
   );
 }
